3d5218779ae4e07129b4cef706eda400f9d5026c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-21 13:53:44 +0900

    fix test to fail at random

--- test/to_fixture_test.rb
+++ test/to_fixture_test.rb
@@ -1,14 +1,10 @@
 require 'test_helper'
 
 class ToFixtureTest < Minitest::Test
-  def teardown
-    User.delete_all
-  end
-
   def test_to_fixture
     user = User.create!(name: 'to_fixture', email: 'to_fixture@example.com')
 
-    expected = "users_1:\n  name: to_fixture\n  email: to_fixture@example.com"
+    expected = "users_#{user.id}:\n  name: to_fixture\n  email: to_fixture@example.com"
     assert_equal expected, user.to_fixture
   end
 
