9cdf04b11f350fc90c6aaec417cb09916d51cb4e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-22 07:31:32 +0900

    do not display migration info

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'minitest/autorun'
 
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 ActiveRecord::Base.logger = Logger.new(nil)
+ActiveRecord::Migration.verbose = false
 
 ActiveRecord::Schema.define do
   create_table :users, force: true do |t|
